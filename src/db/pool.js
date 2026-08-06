c8c8fc618564524b9ce1f0e1af5232c9c45275ad
Drew Ogryzek <drew@vancouvertechpodcast.com>
2026-08-05 18:27:32 -0700

    Adds pool.js

---
+++ src/db/pool.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2/promise');
+const config = require('../config');
+
+const pool = mysql.createPool({
+  host: config.db.host,
+  port: config.db.port,
+  user: config.db.user,
+  password: config.db.password,
+  database: config.db.database,
+  ssl: config.db.ssl,
+  waitForConnections: true,
+  connectionLimit: 10,
+  namedPlaceholders: true,
+});
+
+module.exports = pool;
